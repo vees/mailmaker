e6fca01ee77879d69aed39bfc549eda1bc1cb1c6
Rob Carlson <robvees@fanta.dreamhost.com>
2010-04-01 15:56:00 -0700

    Property listing models

---
+++ properties/admin.py
@@ -0,0 +1,8 @@
+from harfordpark.properties.models import *
+from django.contrib import admin
+
+admin.site.register(Street)
+admin.site.register(Property)
+admin.site.register(Listing)
+admin.site.register(ListingWebPage)
+
