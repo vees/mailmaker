9bb30c189c7f8ec6c234b1cf8c555752d2ca97ec
Rob Carlson <rob@vees.net>
2010-07-02 07:42:02 -0700

    Added mailing list application model

---
+++ mailinglist/admin.py
@@ -0,0 +1,14 @@
+from harfordpark.mailinglist.models import *
+from django.contrib import admin
+
+admin.site.register(PersonEmail)
+admin.site.register(PersonPhone)
+admin.site.register(PersonInterest)
+admin.site.register(AgeGroup)
+admin.site.register(Person)
+admin.site.register(County)
+admin.site.register(Street)
+admin.site.register(Property)
+admin.site.register(UnitType)
+admin.site.register(Unit)
+admin.site.register(ResiProperty)
