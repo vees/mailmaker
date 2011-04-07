395b97f6f5ff2c1273cddc9621cb5866108b3195
Rob Carlson <rob@vees.net>
2011-04-07 17:22:15 -0400

    Working model for data import

---
+++ potluck/admin.py
@@ -0,0 +1,10 @@
+from harfordpark.potluck.models import *
+from django.contrib import admin
+
+admin.site.register(County)
+admin.site.register(Property)
+admin.site.register(Interest)
+admin.site.register(Industry)
+admin.site.register(Household)
+admin.site.register(Person)
+admin.site.register(Business)
--- potluck/models.py
+++ potluck/models.py
@@ -16,7 +16,7 @@ class Property(models.Model):
 		url="http://sdatcert3.resiusa.org/rp_rewrite/details.aspx"
 		return "{0}?AccountNumber={1}County={2}&SearchType=STREET".format(
 			url,
-			urllib.quote(self.account_number),
+			urllib.quote(self.account),
 			self.county.resicode)
 	house = models.CharField(max_length=10, null=False)
 	street = models.CharField(max_length=50, null=False)
@@ -24,14 +24,18 @@ class Property(models.Model):
 	city = models.CharField(max_length=50, null=False)
 	#zip = us.forms.USZipCodeField(null=False)
 	zip = models.CharField(max_length=10, null=False)
-	account = models.CharField(max_length=50, null=False)
+	account = models.CharField(max_length=50, null=True, blank=True)
 	latitude = models.FloatField(blank=True, null=True)
 	longitude = models.FloatField(blank=True, null=True)
 
 class Interest(models.Model):
+	def __unicode__(self):
+		return self.description
 	description = models.CharField(max_length=50)
 
 class Industry(models.Model):
+	def __unicode__(self):
+		return self.description
 	description = models.CharField(max_length=50, null=False, blank=False)
 
 # Membership is stored interally to a household
@@ -41,7 +45,7 @@ class Household(models.Model):
 	def __unicode__(self):
 		# Also need to code for the case where the name is not
 		# defined and we need to use the name of the head instead.
-		return u"{0} {1} Household".format()
+		return u"{0} Household at {1}".format(self.name, self.property.street)
 	MEMBERSHIP_CHOICES = (
 		('R', 'Resident'),
 		('A', 'Auxiliary'),
@@ -64,23 +68,25 @@ class Person(models.Model):
 	head = models.NullBooleanField()
 	firstname = models.CharField(max_length=50)
 	lastname = models.CharField(max_length=50)
-	phone = PhoneNumberField(null=False, unique=False)
-	phone_backup = PhoneNumberField(null=False, unique=False)
-	email = models.EmailField(null=False, unique=False)
-	email_backup = models.EmailField(null=False, unique=False)
-	birthday = models.DateField(auto_now=False, auto_now_add=False)
-	website = models.URLField(verify_exists=True, max_length=200)
+	phone = PhoneNumberField(null=True, blank=True, unique=False)
+	phone_backup = PhoneNumberField(null=True, blank=True, unique=False)
+	email = models.EmailField(null=True, blank=True, unique=False)
+	email_backup = models.EmailField(null=True, blank=True, unique=False)
+	birthday = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True, unique=False)
+	website = models.URLField(verify_exists=True, max_length=200, null=True, blank=True, unique=False)
 	interests = models.ManyToManyField(Interest, null=True, blank=True)
-	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-	household = models.ForeignKey(Household, unique=False, null=False, blank=False)
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True, unique=False)
+	household = models.ForeignKey(Household, unique=False, null=True, blank=True)
 
 class Business(models.Model):
+	def __unicode__(self):
+		return self.name
 	name = models.CharField(max_length=50, null=True, blank=True)
 	owner = models.ForeignKey(Person, unique=True, null=True, blank=True,
 		related_name='owner_set')
-	phone = PhoneNumberField(null=True, unique=False)
-	email = models.EmailField(null=True, unique=False)
-	website = models.URLField(verify_exists=True, max_length=200)
+	phone = PhoneNumberField(null=True, blank=True, unique=False)
+	email = models.EmailField(null=True, blank=True, unique=False)
+	website = models.URLField(verify_exists=True, null=True, blank=True, max_length=200)
 	location = models.ForeignKey(Property, unique=False, null=False, blank=False)
 	address_append = models.CharField(max_length=50, null=True, blank=True)
 	industries = models.ManyToManyField(Industry, null=True, blank=True)
