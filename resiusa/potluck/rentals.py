89e0b12dd8ea4f3eb6bc886a9a85cd70b70523cf
Rob Carlson <rob@vees.net>
2013-12-07 20:40:17 -0500

    Moved resiusa directories as peers to other projects

--- resiusa/potluck/rentals.py
+++
@@ -1,6 +0,0 @@
-import csv
-from potluck.models import *
-import sys
-sys.path.append("/home/robvees/harfordpark.com") 
-
-
--- resiusa/properties/admin.py
+++
@@ -1,8 +0,0 @@
-from harfordpark.properties.models import *
-from django.contrib import admin
-
-admin.site.register(Street)
-admin.site.register(Property)
-admin.site.register(Listing)
-admin.site.register(ListingWebPage)
-
